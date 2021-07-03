561fcc32b33e9c3ea75322e31fad1fde941f8724
TylerJudson <TylerJudson23@gmail.com>
2021-07-03 11:08:13 -0600

    Star SHower

--- JavascriptGames/StarShower/src/index.js
+++ JavascriptGames/StarShower/src/index.js
@@ -1,129 +1,184 @@
-import { Display } from "./display.js";
-import { Engine } from "./engine.js";
-import { World } from "./world/world.js";
 
-let world;
-let display;
-let engine;
 
-function init() {
-    world = new World();
-    display = new Display(world.width, world.height);
-    engine = new Engine(update, render)
-    engine.start();
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let stars, particleStars, w, h, cx, cy
+
+
+
+
+//#region Objects
+class Star {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = {
+            x: 0,
+            y: 3
+        }
+        this.friction = 0.8;
+        this.gravity = 1;
+    }
+    shatter() {
+        this.radius -= 3;
+        for (let i = 0; i < 8; i ++) {
+            particleStars.push(new StarParticle(this.x, this.y, 2))
+        }
+    }
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+    update() {
+        this.draw();
+
+        // When ball hits the bottom of the screen
+        if (this.y + this.radius + this.velocity.y > h) {
+            this.velocity.y = -this.velocity.y * this.friction;
+            this.shatter();
+        }
+        else {
+            this.velocity.y += this.gravity;
+        }
+        this.y += this.velocity.y;
+    }
 }
-function update() {
-    world.update();
+
+class StarParticle {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = {
+            x: getRandomInt(-5, 5),
+            y: getRandomInt(-15, 15)
+        }
+        this.friction = 0.8;
+        this.gravity = 0.25;
+        this.ttl = 100; // time to live
+        this.opacity = 1;
+    }
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = `rgba(255, 0, 0, ${this.opacity})`;
+        ctx.fill();
+        ctx.closePath();
+    }
+    update() {
+        this.draw();
+
+        // When ball hits the bottom of the screen
+        if (this.y + this.radius + this.velocity.y > h) {
+            this.velocity.y = -this.velocity.y * this.friction;
+        }
+        else {
+            this.velocity.y += this.gravity;
+        }
+        this.y += this.velocity.y;
+        this.x += this.velocity.x;
+        this.ttl -= 1;
+        this.opacity -= 1 / this.ttl;
+    }
 }
-function render() {
-    world.renderBackground(display.ctx);
-    world.render(display.ctx);
-}
 
-
-
-//#region Functions
-
-// function drawBackground() {
-//     drawSky();
-//     drawStars();
-//     drawMountains();
-// }
-// function drawStars() {
-//     for (let i = 0; i <= getRandomInt(200, 500); i++) {
-//         drawStar(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(2, 15));
-//     }
-// }
-// function drawStar(x, y, r) {
-//     ctx.save();
-
-//     let gradient = ctx.createRadialGradient(x, y, r/5, x, y, r);
-//     gradient.addColorStop(0, "white");
-//     gradient.addColorStop(.1, "rgba(255, 255, 255, .3)")
-//     gradient.addColorStop(1, "rgba(255, 255, 255, 0)")
-
-//     ctx.beginPath();
-//     ctx.arc(x, y, r, 0, Math.PI*2);
-//     ctx.fillStyle = gradient;
-//     ctx.fill();
-
-//     ctx.restore();
-// }
-// function drawSky() {
-//     ctx.save();
-
-//     let gradient = ctx.createRadialGradient(centerX, height, 300, centerX, centerY, width);
-//     gradient.addColorStop(0, "rgb(0, 0, 0)");
-//     gradient.addColorStop(1, "rgb(0, 0, 50)");
-
-
-//     ctx.fillStyle = gradient;
-//     ctx.fillRect(0, 0, width, height);
-//     ctx.restore();
-// }
-// function drawMountains() {
-//     ctx.save();
-
-//     let colorValue = getRandomInt(30, 40);
-//     let gradient = ctx.createLinearGradient(width/2, 0, width/2, height);
-//     gradient.addColorStop(0, `rgb(${colorValue}, ${colorValue}, ${colorValue})`);
-//     gradient.addColorStop(1, `rgb(${colorValue - 10}, ${colorValue - 10}, ${colorValue-10})`);
-
-//     for (let i = 0; i <= getRandomInt(1, 3); i++) {
-//         let x1 = getRandomInt(-400, width/3 * 2);
-//         let x2 = getRandomInt(x1 + 1000, x1 + 2000);
-//         let h = getRandomInt(50, 300);
-//         drawPointedMountain(x1, x2, h, gradient);
-//     }
-
-//     colorValue = getRandomInt(20, 30);
-//     gradient = ctx.createLinearGradient(width/2, 0, width/2, height);
-//     gradient.addColorStop(0, `rgb(${colorValue}, ${colorValue}, ${colorValue})`);
-//     gradient.addColorStop(1, `rgb(${colorValue - 10}, ${colorValue - 10}, ${colorValue - 10})`);
-//     for (let i = 0; i <= getRandomInt(3, 10); i++) {
-//         let x1 = getRandomInt(-300, width/4 * 3);
-//         let x2 = getRandomInt(x1 + 500, x1 + 1500);
-//         let h = getRandomInt(400, 600);
-//         drawPointedMountain(x1, x2, h, gradient);
-//     }
-
-//     colorValue = getRandomInt(10, 20);
-//     gradient = ctx.createLinearGradient(width/2, 0, width/2, height);
-//     gradient.addColorStop(0, `rgb(${colorValue}, ${colorValue}, ${colorValue})`);
-//     gradient.addColorStop(1, `rgb(${colorValue - 10}, ${colorValue - 10}, ${colorValue - 10})`);
-//     for (let i = 0; i <= getRandomInt(5, 15); i++) {
-//         let x1 = getRandomInt(-200, width - 100);
-//         let x2 = getRandomInt(x1 + 300, x1 + 1000)
-//         let h = getRandomInt(600, 800);
-//         drawPointedMountain(x1, x2, h, gradient);
-//     }
-
-
-//     ctx.restore();
-// }
-// function drawPointedMountain(x1, x2, h, color) {
-//     ctx.save();
-//     ctx.beginPath();
-//     ctx.moveTo(x1, height);
-//     ctx.lineTo((x1 + x2)/2, h);
-//     ctx.lineTo(x2, height);
-//     ctx.fillStyle = color;
-//     ctx.fill()
-
-//     ctx.restore();
-// }
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min) + min);
-// }
 //#endregion
 
 
+//#region Functions
+function init() {
+    w = canvas.width;
+    h = canvas.height;
+    cx = w/2;
+    cy = h/2;
+
+    stars = [];
+    particleStars = [];
+
+    for (let i = 0; i < 1; i++) {
+        stars.push(new Star(cx, 30, 30, "blue"));
+    }
+}
+function animate() {
+    requestAnimationFrame(animate);
+    clear();
+    updateStars();
+    updateParticleStars();
+}
+function clear() {
+    ctx.clearRect(0, 0, w, h);
+}
+function updateStars() {
+    stars.forEach(star => {
+        star.update();
+        if (star.radius <= 0) {
+            remove(star, stars);
+        }
+    });
+}
+function updateParticleStars() {
+    particleStars.forEach(particle => {
+        particle.update();
+        if (particle.ttl <= 0) {
+            remove(particle, particleStars)
+        }
+        
+    });
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function distance(x1, y1, x2, y2) {
+    const deltaX = x2 - x1;
+    const deltaY = y2 - y1;
+
+    return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+}
+function remove(element, array) {
+    array.splice(array.indexOf(element), 1)
+}
+init();
+animate();
 
 
 
-onload = init;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 addEventListener("resize", () => {
-    display.resize();
-});
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+
+    init();
+})
