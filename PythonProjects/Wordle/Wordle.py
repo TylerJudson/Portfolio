f2de437f3a799ed746bdc6fc7e0034dc7638e0a2
TylerJudson <tylerjudson23@gmail.com>
2022-03-25 17:19:39 -0600

    Finished Words

--- PythonProjects/Wordle/Wordle.py
+++ PythonProjects/Wordle/Wordle.py
@@ -1,8 +1,4 @@
-from graphlib import CycleError
-from textwrap import fill
-from time import sleep
-from turtle import fillcolor
-from typing import List, Tuple
+from typing import Tuple
 import pygame
 from pygame.locals import *
 from Alert import Alert
@@ -19,7 +15,7 @@ class Wordle:
 	"""A class to play the game Wordle
 	"""
 
-	size = (400, 600)
+	size = (400, 700)
 	"""The size of the game window"""
 	width = size[0]
 	"""The width of the game window"""
@@ -54,7 +50,7 @@ class Wordle:
 		title = Text((self.width / 2, 100), self.font, 75, "WORDLE", WHITE)
 
 		# Create the Log in button for the startScreen
-		logInButton = Button(Surface((self.width/ 4 - 75 + 5, 400), (150, 50), self.backgroundColor),
+		logInButton = Button(Surface((self.width/ 4 - 75 + 5, 550), (150, 50), self.backgroundColor),
 							Style(Text((75, 25), self.font, 25, "LOG IN", DENIM),
 							fillColor=self.backgroundColor, borderColor=DENIM, borderRadius=5),
 
@@ -62,7 +58,7 @@ class Wordle:
 							fillColor=DENIM, borderColor=DENIM, borderRadius=5))
 
 		# Create the sign up button for the startScreen
-		signUpButton = Button(Surface((self.width * 3 / 4 - 75 - 5, 400), (150, 50), self.backgroundColor),
+		signUpButton = Button(Surface((self.width * 3 / 4 - 75 - 5, 550), (150, 50), self.backgroundColor),
 								Style(Text((75, 25), self.font, 24, "SIGN UP", ORCHID),
 								fillColor=self.backgroundColor, borderColor=ORCHID, borderRadius=5),
 
@@ -174,7 +170,8 @@ class Wordle:
                          fillColor=DENIM, borderRadius=5))
 
 
-
+		# Create the keyboard
+		keyboard = self.createKeyboard(175, (5, 7))
 		
 		# create the potential alert message for the screen
 		alert = Alert(Surface((-1, -1), (0, 0), self.backgroundColor), Text((0, 0), None, 0, "", (0, 0, 0)))
@@ -299,6 +296,13 @@ class Wordle:
 			# render the login button
 			logInButton.render(mousePos)
 			logInScreen.display.blit(logInButton.surface.display, logInButton.surface.pos)
+
+			# render the keyboard
+			for key in keyboard:
+				keyboard[key].render(mousePos)
+				logInScreen.display.blit(keyboard[key].surface.display, keyboard[key].surface.pos)
+			
+
 
 			# render the alert
 			alert.render(mousePos)
@@ -538,6 +542,8 @@ class Wordle:
 		"""Plays the game"""
 		# create the game in screen
 		gameScreen = Surface((0, 0), self.size, self.backgroundColor)
+		gameover = False
+		win = False
 
 		# Create the header for the screen
 
@@ -550,16 +556,50 @@ class Wordle:
                              fillColor=CYBERGRAPE))
 
 		# the words for the game
-		words = [[""] * 6]
+		secretWord = "TTAAA"
+		words = [""] * 6
+		currentWord = 0
+		greenLetters = ""
+		yellowLetters = ""
+		blackLetters = ""
 
+		keyboard = self.createKeyboard(175, (5, 7))
 
 
 		alert = Alert(Surface((-1, -1), (0, 0),
 		              self.backgroundColor), Text((0, 0), None, 0, "", (0, 0, 0)))
 
+
+		endScreen = Surface((self.width / 2 - 150, self.height / 2 - 200), (300, 400), self.backgroundColor)
+		# display the play agian button
+		playAgainBtn = Button(Surface((10, 340), (125, 50), self.backgroundColor),
+								Style(Text((125 / 2, 25), self.font, 15, "PLAY AGAIN", DENIM), borderColor=DENIM, borderRadius=5),
+								hoverStyle=Style(Text((125 / 2, 25), self.font, 17, "PLAY AGAIN", WHITE), fillColor=DENIM, borderRadius=5))
+		
+
+		# display the exit button
+		exitBtn = Button(Surface((165, 340), (125, 50), self.backgroundColor),
+								Style(Text((125 / 2, 25), self.font, 15, "EXIT", LIGHTRED), borderColor=LIGHTRED, borderRadius=5),
+								hoverStyle=Style(Text((125 / 2, 25), self.font, 17, "EXIT", WHITE), fillColor=LIGHTRED, borderRadius=5))
+		
+
 		while True:
 			# run at 60 fps
 			self.clock.tick(60)
+
+
+
+			# check for game over
+			if (not gameover and words[currentWord - 1] == secretWord):
+				pygame.time.delay(2000)
+				gameover = True
+				win = True
+			elif (not gameover and currentWord == 6):
+				pygame.time.delay(2000)
+				gameover = True
+
+
+			
 
 			# clear the screen
 			gameScreen.clear()
@@ -585,14 +625,54 @@ class Wordle:
 					elif (alert.mouseClickClose(mousePos)):
 						alert = Alert(Surface((-1, -1), (0, 0),
 						              self.backgroundColor), Text((0, 0), None, 0, "", (0, 0, 0)))
+					
+					# Check to see if the mouse clicked on the keyboard
+					elif (not gameover):
+						# loop throught the keys and check to see if the mouse clicked them
+						for key in keyboard:
+							if (keyboard[key].mouseIsHovering(mousePos)):
+								# if the key is the enter button encrement the current word
+								if (key == "ENTER"):
+									if (len(words[currentWord]) == 5):
+										currentWord += 1
+								# if the key is the back button delete the last character of the current word
+								elif (key == "BACK"):
+									words[currentWord] = words[currentWord][:-1]
+								# else just add the word if the length is less than 5
+								elif (len(words[currentWord]) < 5):
+									words[currentWord] += key
 
+					# Check to see if the mouse clicked the play again button
+					elif (playAgainBtn.mouseIsHovering((mousePos[0] - endScreen.pos[0], mousePos[1] - endScreen.pos[1]))):
+						self.Play()
+						return
+					
+					# Check to see if the mouse clicked the eixt button
+					elif (exitBtn.mouseIsHovering((mousePos[0] - endScreen.pos[0], mousePos[1] - endScreen.pos[1]))):
+						self.Start()
+						return
 
-			# create the rectangles
+				# Checks for the KEYDOWN event
+				if event.type == pygame.KEYDOWN and not gameover:
+					# if the return key is pressed
+					if event.key == pygame.K_RETURN:
+						if (len(words[currentWord]) == 5):
+							currentWord += 1
+					# if the backspace key is pressed
+					elif event.key == pygame.K_BACKSPACE:
+						words[currentWord] = words[currentWord][:-1]
+					# else just add to the word if the length is less than 5
+					elif (len(words[currentWord]) < 5):
+						words[currentWord] += event.unicode.upper()
+
+			# create the aesthetic lines
 			pygame.draw.rect(gameScreen.display, LIGHTGREEN,
 			                 (-33, 69, 320, 700), 2, 10)
-			pygame.draw.rect(gameScreen.display, ORCHID, (130, 200, 600, 600), 2, 10)
+			pygame.draw.rect(gameScreen.display, ORCHID, 
+							 (130, 200, 600, 600), 2, 10)
 			pygame.draw.rect(gameScreen.display, CYBERGRAPE,
 			                 (-33, 500, 600, 300), 2, 10)
+							 
 
 			# render the header
 			titleButton.render()
@@ -600,65 +680,89 @@ class Wordle:
 			gameScreen.display.blit(header.display, header.pos)
 
 
+			# Create the word boxes
+			wordsHeight = 400
+			padding = 40
+			margin = 8
+			boxSize = (wordsHeight - padding * 2 - margin * 4) / 5
+			for i in range(0, len(words)):
+				for j in range(0, 5):
+					# get the text from the words
+					text = words[i][j] if j < len(words[i]) else ""
 
-			# Create the words in the squares
-			# wordBlocks = Surface((0, 75), (self.width, 400), DENIM)
-			# for i in range(0, 6):
-			# 	y = wordBlocks.display.get_rect().height * i / 6
-			# 	row = Surface((0, y), (wordBlocks.width, wordBlocks.height / 6), ORCHID)
+					
+					
+					# determine the fill color of the letter
+					fillColor = self.backgroundColor
+					# only show the color if we have entered the word
+					if (i < currentWord):
+						# if the indexes match turn the fill color to green and add the letter to green letters
+						if (secretWord[j] == text):
+							fillColor = GREEN
+							# if the text is not already in the greenletters add it
+							if (text not in greenLetters):
+								greenLetters += text
+						# if the text is in the secretword turn the fill color to yellow and add the letter to yellow letters
+						elif (text in secretWord and text != ""):
+							fillColor = YELLOW
+							# if the text is not already in the yellowletters add it
+							if (text not in yellowLetters):
+								yellowLetters += text
+						else:
+							fillColor = DARKGRAY
+							if (text not in blackLetters):
+								blackLetters += text
 
+					box = Button(Surface((padding + j * (boxSize + margin), i * (boxSize + margin) + 75), (boxSize, boxSize), self.backgroundColor), Style(Text(
+							(boxSize / 2, boxSize / 2), self.font, 25, text, WHITE), borderColor=WHITE, borderRadius=5, fillColor=fillColor))
+					box.render()
+					gameScreen.display.blit(box.surface.display, box.surface.pos)
 
-			# 	for j in range(0, 5):
-			# 		x = (row.display.get_rect().width) * j / 5
-			# 		side = row.height
-			# 		box = Button((x, 0), Surface((0, 0), (side, side), self.backgroundColor), Style(Text(
-			# 			(side / 2, side / 2), self.font, 25, "W", WHITE), borderColor=WHITE, borderRadius=5))
-			# 		box.render()
-			# 		row.display.blit(box.surface.display, box.pos)
 			
-			# 	wordBlocks.display.blit(row.display, row.pos)
+
+			# render the keyboard
+			for key in keyboard:
+				if (key in greenLetters):
+					keyboard[key].style.fillColor = GREEN
+					keyboard[key].hoverStyle.fillColor = DARKGREEN
+				elif (key in yellowLetters):
+					keyboard[key].style.fillColor = YELLOW
+					keyboard[key].hoverStyle.fillColor = DARKYELLOW
+				elif (key in blackLetters):
+					keyboard[key].style.fillColor = DARKGRAY
+					keyboard[key].hoverStyle.fillColor = DARKERGRAY
+
+				
+				keyboard[key].render(mousePos)
+				gameScreen.display.blit(keyboard[key].surface.display, keyboard[key].surface.pos)
+			
+
+			# display the end screen if game over
+			if (gameover):
+				endScreen.clear()
+
+				# display the title
+				if (win):
+					text = Text((endScreen.width / 2, 50), self.font, 50, "VICTORY!", LIGHTGREEN)
+				else:
+					text = Text((endScreen.width / 2, 50), self.font, 50, "DEFEAT", (255, 0, 0))
+				endScreen.display.blit(text.display, text.rect)
 
 
-			# gameScreen.display.blit(wordBlocks.display, wordBlocks.pos)
+				# render play again button
+				playAgainBtn.render((mousePos[0] - endScreen.pos[0], mousePos[1] - endScreen.pos[1]))
+				endScreen.display.blit(playAgainBtn.surface.display, playAgainBtn.surface.pos)
 
-			# make the keyboard
-			keyBoardHeight = 175
-			marginx = 5
-			marginy = 7
-			keyHeight = (keyBoardHeight - marginy * 3) / 3
-			keyWidth = (self.width - marginx * 11) / 10
+				# render exit button
+				exitBtn.render((mousePos[0] - endScreen.pos[0], mousePos[1] - endScreen.pos[1]))
+				endScreen.display.blit(exitBtn.surface.display, exitBtn.surface.pos)
+				
 
-			qRow = "QWERTYUIOP"
-			for i, letter in enumerate(qRow):
-				key = Button(Surface((i * keyWidth + (i + 1) * marginx, self.height - keyBoardHeight), (keyWidth, keyHeight), self.backgroundColor),
-							Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY))
-				key.render()
-				gameScreen.display.blit(key.surface.display, key.surface.pos)
-
-
-			aRow = "ASDFGHJKL"
-			for i, letter in enumerate(aRow):
-				key = Button(Surface((i * keyWidth + ((i + 1) * marginx + marginx / 2) + (keyWidth / 2), self.height - keyBoardHeight * 2 / 3), (keyWidth, keyHeight), self.backgroundColor),
-							Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY))
-				key.render()
-				gameScreen.display.blit(key.surface.display, key.surface.pos)
-
-			zRow = "ZXCVBNM"
-			for i, letter in enumerate(zRow):
-				key = Button(Surface((i * keyWidth + ((i + 1) * marginx + marginx * 3 / 2) + keyWidth * 3 / 2, self.height - keyBoardHeight / 3), (keyWidth, keyHeight), self.backgroundColor),
-							Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY))
-				key.render()
-				gameScreen.display.blit(key.surface.display, key.surface.pos)
-
-			enterButton = Button(Surface((marginx + 2, self.height - keyBoardHeight / 3), (keyWidth * 3 / 2, keyHeight), self.backgroundColor),
-                            Style(Text((keyWidth * 3 / 4, keyHeight / 2), self.font, 12, "ENTER", WHITE), borderRadius=3, fillColor=LIGHTGRAY))
-			enterButton.render()
-			gameScreen.display.blit(enterButton.surface.display, enterButton.surface.pos)
-
-			# backButton = Button(Surface((marginx, self.height - keyBoardHeight / 3), (keyWidth, keyHeight), self.backgroundColor),
-            #                 Style(Text((keyWidth / 2, keyHeight / 2), self.font, 10, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY))
-			# #backButton.render()
-			# gameScreen.display.blit(enterButton.surface.display, enterButton.surface.pos)
+				# create a layover to hide the other screen
+				layOver = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+				layOver.fill((0, 0, 0, 128))
+				gameScreen.display.blit(layOver, (0,0))
+				gameScreen.display.blit(endScreen.display, endScreen.pos)
 
 			# render the alert
 			alert.render(mousePos)
@@ -678,6 +782,9 @@ class Wordle:
 		Args:
 			username (str): The username to compare the password to
 			password (str): The password for the username
+
+		Returns:
+			Tuple[bool, Alert]: [0] whether it was a success or not, [1] the alert to display on fail
 		"""
 		
 		error = ""
@@ -707,6 +814,9 @@ class Wordle:
 			username (str): the username for the user
 			password (str): The password for the user
 			verifyPassword (str): The password verification for the user
+
+		Returns:
+			Tuple[bool, Alert]: [0] whether it was a success or not, [1] the alert to display on fail
 		"""
 		error = ""
 		type = "Warning"
@@ -735,6 +845,50 @@ class Wordle:
 
 		return[False, Alert(Surface((self.width / 2 - 350 / 2, 80), (350, 100), self.backgroundColor),
 									Text((145, 50), self.font, fontSize, error, BLACK), type)]
+
+	def createKeyboard(self, keyboardHeight: int, margin: Tuple[int, int]) -> dict[str, Button]:
+		"""Returns a keyboard with the specified hieght at the bottom of the screen
+
+		Args:
+			keyBoardHeight (int): the height of the keyboard
+			margin (Tuple[int, int]): The space between the keys. margin[0] is x and margin[1] is y.
+
+		Returns:
+			Dict(str, Button): The keyboard. Dict[letter] = button
+		"""
+
+		keyboard = {}
+
+		keyHeight = (keyboardHeight - margin[1] * 3) / 3
+		keyWidth = (self.width - margin[0] * 11) / 10
+
+
+		qRow = "QWERTYUIOP"
+		for i, letter in enumerate(qRow):
+			keyboard.update({letter: Button(Surface((i * keyWidth + (i + 1) * margin[0], self.height - keyboardHeight), (keyWidth, keyHeight), self.backgroundColor),
+						Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY),
+						hoverStyle = Style(Text((keyWidth / 2, keyHeight / 2), self.font, 15, letter, WHITE), borderRadius=3, fillColor=GRAY))})
+
+		aRow = "ASDFGHJKL"
+		for i, letter in enumerate(aRow):
+			keyboard.update({letter: Button(Surface((i * keyWidth + ((i + 1) * margin[0] + margin[0] / 2) + (keyWidth / 2), self.height - keyboardHeight * 2 / 3), (keyWidth, keyHeight), self.backgroundColor),
+						Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY),
+						hoverStyle = Style(Text((keyWidth / 2, keyHeight / 2), self.font, 15, letter, WHITE), borderRadius=3, fillColor=GRAY))})
+
+		zRow = "ZXCVBNM"
+		keyboard.update({"ENTER": Button(Surface((margin[0] + 2, self.height - keyboardHeight / 3), (keyWidth * 3 / 2, keyHeight), self.backgroundColor),
+						Style(Text((keyWidth * 3 / 4, keyHeight / 2), self.font, 12, "ENTER", WHITE), borderRadius=3, fillColor=LIGHTGRAY),
+						hoverStyle = Style(Text((keyWidth * 3 / 4, keyHeight / 2), self.font, 13, "ENTER", WHITE), borderRadius=3, fillColor=GRAY))})
+		for i, letter in enumerate(zRow):
+			keyboard.update({letter: Button(Surface((i * keyWidth + ((i + 1) * margin[0] + margin[0] * 3 / 2) + keyWidth * 3 / 2, self.height - keyboardHeight / 3), (keyWidth, keyHeight), self.backgroundColor),
+						Style(Text((keyWidth / 2, keyHeight / 2), self.font, 14, letter, WHITE), borderRadius=3, fillColor=LIGHTGRAY),
+						hoverStyle = Style(Text((keyWidth / 2, keyHeight / 2), self.font, 15, letter, WHITE), borderRadius=3, fillColor=GRAY))})
+
+		keyboard.update({"BACK": Button(Surface((keyWidth * 17 / 2 + margin[0] * 9 + 2, self.height - keyboardHeight / 3), (keyWidth * 3 / 2, keyHeight), self.backgroundColor),
+					Style(Text((keyWidth * 3 / 4, keyHeight / 2), self.font, 12, "BACK", WHITE), borderRadius=3, fillColor=LIGHTGRAY),
+					hoverStyle = Style(Text((keyWidth * 3 / 4, keyHeight / 2), self.font, 13, "BACK", WHITE), borderRadius=3, fillColor=GRAY))})
+
+		return keyboard
 
 							
 wordle = Wordle()
